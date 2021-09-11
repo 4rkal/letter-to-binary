6ebbcd22d7b50fcd05a6522bc9d98151de34b85b
Arkal <89646772+4rkal@users.noreply.github.com>
2021-09-11 15:51:21 +0300

    main.py
    quite old code so might have some mistakes

---
+++ main.py
@@ -0,0 +1,45 @@
+while True:
+ print()
+ print("---------------------------------------------------------")
+ print("            WELCOME TO BINARY CODE TRANSLATOR            ")
+ print("---------------------------------------------------------")
+ print()
+ a=input("         ENTER A CAPITAL LETER TO TRANSLATE:           ")
+ if a =="A":
+  print("01000001")
+ elif a =="B":
+  print("01000010")
+ elif a =="C":
+  print("01000011")
+ elif a =="D":
+  print("01000100")
+ elif a =="E":
+  print("01000101")
+ elif a =="F":
+  print("01000110")
+ elif a =="G":
+  print("01000111")
+ elif a =="H":
+  print("01001000")
+ elif a =="I":
+  print("01001001")#
+ elif a =="G":
+  print("01000111")
+ elif a =="K":
+  print("01001011")
+ elif a =="L":
+  print("01001100")
+ elif a =="M":
+  print("01001101")
+ elif a =="N":
+  print("01001110")
+ elif a =="O":
+  print("01001111")
+ elif a =="P":
+  print("01010000")
+ elif a =="Q":
+  print("01010001")
+ elif a =="R":
+  print("01010010")
+else:
+    print("DID NOT FIND",a)
